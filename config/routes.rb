90fb5b59ca061ab7a60a8150732da85cc8c47df0
jeffdemers <jeffdemers@gmail.com>
2015-07-03 08:34:13 -0700

    initial commit app with authentication

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'pages/index'
+
+  root to: 'pages#index'
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
+
+
+
+end
