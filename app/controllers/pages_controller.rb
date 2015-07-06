3f33dc297ae6e1ac551735e9113e6ae00c9a09fd
jeffdemers <jeffdemers@gmail.com>
2015-07-06 16:19:53 -0700

    updates made to styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,4 @@ class PagesController < ApplicationController
   def index
   end
 
-
-
 end
--- app/models/journal.rb
+++ app/models/journal.rb
@@ -2,8 +2,12 @@ class Journal < ActiveRecord::Base
   has_many :entries
   belongs_to :user
 
+  # has_attached_file :image, :styles => { :medium => "300x300>"}
+  # validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   # attr_accessible :content, :title, :rank, :user
 
   validates :content, :presence => true, :length => { :minimum => 20 }
   validates :title, :presence => true
 end
+
